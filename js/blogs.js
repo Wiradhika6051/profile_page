7c1ec2438c801b7c0cc84e51037a1ab25a768b25
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-09-03 07:33:53 +0700

    feat: add basic blog list

---
+++ js/blogs.js
@@ -0,0 +1,24 @@
+import blogs from '../data/blogs.json' with {type:"json"}
+
+const blogsRoot = document.querySelector("#blogs");
+
+// render list of blog
+renderBlogList(blogs);
+
+
+function renderBlogList(blogs){
+  const blogList = blogs.map((blog)=>`
+  <a href="../blogs/${blog.content_path}" class="blog-link">
+    <div class="blog">
+      <div class="blog-header">
+        <h3>${blog.title}</h3>
+        <p>${blog.date}</p>
+      </div>
+      <div class="blog-desc">
+        <p>${blog.desc}</p>
+      </div>
+    </div>
+  </a>
+  `);
+  blogsRoot.innerHTML = blogList.join("");
+}
