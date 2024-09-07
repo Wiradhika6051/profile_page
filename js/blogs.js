8c08130a6d50386d9c0253cf1ff58f8e919b79f0
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-09-07 10:41:32 +0700

    feat: format date

--- js/blogs.js
+++ js/blogs.js
@@ -7,12 +7,13 @@ renderBlogList(blogs);
 
 
 function renderBlogList(blogs){
+  const options = {day:'2-digit','month':'long','year':'numeric'};
   const blogList = blogs.map((blog)=>`
   <a href="../blogs/${blog.content_path}" class="blog-link">
     <div class="blog">
       <div class="blog-header">
         <h3>${blog.title}</h3>
-        <p class="date">${blog.date}</p>
+        <p class="date">${new Date(blog.date).toLocaleDateString('en-GB',options)}</p>
       </div>
       <div class="blog-desc">
         <p>${blog.desc}</p>
