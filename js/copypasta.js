2bf7564704f069e922c74f4c1d5c07f99d18b97a
Fawwaz Anugrah Wiradhika Dharmasatya <anugrahdwfawwaz@gmail.com>
2024-12-03 22:37:36 +0700

    feat: add event listener when clicking copypasta list

--- js/copypasta.js
+++ js/copypasta.js
@@ -1,17 +1,30 @@
 import copypasta from "../data/copypasta.json" with {type:"json"}
 
 const copypastaRoot= document.querySelector("#copypasta")
+const detailRoot = document.querySelector("#detail")
 
 renderCopypastaList(copypasta)
 
+function showDetail(id){
+  console.log(id)
+  console.log(copypasta.filter(c=>c.id===id))
+}
+
 function renderCopypastaList(copypasta){
   console.log(copypasta)
-  const copypastaList = copypasta.map((c)=>`<div class="project copypasta-info">
+  const copypastaList = copypasta.map((c)=>`<div class="project copypasta-info"  data-id=${c.id}>
         <div class="desc copypasta-box">
           <h2>${c.name}</h2>
           <p>${c.description}</p>
         </div>
       </div>`)
   copypastaRoot.innerHTML = copypastaList.join("")
-}
-
+  // add listener
+  const copypastaInfos = document.querySelectorAll(".copypasta-info")
+  copypastaInfos.forEach((c)=>{
+    c.addEventListener("click",()=>{
+      const id= parseInt(c.getAttribute("data-id"))
+      showDetail(id)
+    })
+  })
+  }
