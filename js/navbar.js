d698cb3d19ba640e1bbf49e473b16d4b0d7dde9f
Wiradhika6051 <anugrahdwfawwaz@gmail.com>
2025-08-09 17:22:28 +0700

    fix: fix menu list glitch when resize happen

--- js/navbar.js
+++ js/navbar.js
@@ -19,3 +19,18 @@ document.addEventListener("click",function(event){
         showTabs()
     }
 })
+
+window.addEventListener("resize",function(event){
+    const tabs = document.getElementById("tabs")
+    const bar = document.querySelector("a.icon")
+    if (window.innerWidth>500){
+        console.log("resize",window.innerWidth,bar.classList.contains("open"))
+        if(bar.classList.contains("open")){
+            bar.classList.remove("open")
+        }
+        tabs.style.display= 'flex'
+    }
+    else{
+        tabs.style.display= 'none'
+    }
+})
