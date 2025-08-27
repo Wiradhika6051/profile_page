12b3774c3ce53c9597bfd0cc2973c9da28bf28fc
Wiradhika6051 <anugrahdwfawwaz@gmail.com>
2025-08-28 05:56:29 +0700

    refactor: refactor sortData

--- js/sortButton.js
+++ js/sortButton.js
@@ -1,32 +1,30 @@
-
-// Sorting modes
-
 export default function sortData(datas,mode,label,renderer){
+    const {key,order} = mode
+    const direction = order === "asc" ? 1 : -1;
+
     datas.sort((a,b)=>{
-        let valA = a[mode.key]
-        let valB = b[mode.key]
+        let valA = a[key]
+        let valB = b[key]
+
         // Kalau date convert ke Date objects
-        if(mode.key==='date'){
+        if(key==='date'){
             valA = new Date(valA)
             valB = new Date(valB)
         }
-        // karena kalau date gak bisa pakai ===, kita cek dari valA dan valB nya aja
-        if (valA < valB){
-            return mode.order==='asc'? -1 : 1;
+
+        if(valA===valB){
+            return 0;
         }
-        if (valA > valB){
-            return mode.order==='asc'? 1 : -1;
-        }
-        // sisanya berarti sama
-        return 0;
+        return valA < valB ? -1 * direction : 1 * direction
     })
+
+    // Update UI
     updateUI(label)
     // render ulang label
     renderer(datas)
 }
 
 function updateUI(label){
-    // Close dropdown
     // Update button label
     document.getElementById("sortText").innerText = `Sort: ${label}`;
 }
