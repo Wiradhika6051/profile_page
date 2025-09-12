6c479224e64f1a960a0926bac5b43486cc8371df
Wiradhika6051 <anugrahdwfawwaz@gmail.com>
2025-09-12 11:26:37 +0700

    feat: make multiline input reflect in the output as multiline

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,14 @@
-export default function capitalize(val) {
+export function capitalize(val) {
     return String(val)
             .split(/\s+/) // menangani space > 1
             .map(w => w.charAt(0).toUpperCase() + w.slice(1))
             .join(" ");
+}
+
+export function escapeHTML(text) {
+  return text
+    .replace(/[&<>"'/]/g, c => ({
+      "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;",
+      "'": "&#39;", "/": "&#x2F;"
+    }[c]))
 }
